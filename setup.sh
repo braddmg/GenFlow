c05f3207e3a07dcf32b5734a0720a93e3ec81114
Bradd mendoza <56454173+braddmg@users.noreply.github.com>
2024-12-23 00:03:28 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,9 +6,6 @@ echo "Just wait, we're casting some digital spells to make everything run smooth
 # Suppress all output except errors
 exec > /dev/null 2>&1
 
-# Get the current Conda environment path
-CONDA_ENV_PATH=$(conda info --base)/envs/$(basename "$CONDA_PREFIX")
-
 # Download and install necessary packages
 curl -L https://github.com/merenlab/anvio/releases/download/v8/anvio-8.tar.gz --output anvio-8.tar.gz
 wget https://files.pythonhosted.org/packages/c4/aa/94c42a2dd30c4923bffe3ab59e4416c3f7e72dbd32a89bcdd8d43ff1d5d7/datrie-0.8.2-pp373-pypy36_pp73-win32.whl
@@ -24,31 +21,11 @@ pip install matplotlib
 if ! command -v unzip &> /dev/null; then
     sudo apt-get update -qq > /dev/null 2>&1 && sudo apt-get install -y unzip -qq > /dev/null 2>&1;
 fi
-
-# Create directories for activation and deactivation scripts
-mkdir -p "$CONDA_ENV_PATH/etc/conda/activate.d"
-mkdir -p "$CONDA_ENV_PATH/etc/conda/deactivate.d"
-mkdir -p "$CONDA_ENV_PATH/scripts"
-
-# Copy the activation and deactivation scripts
-cp scripts/variables.sh "$CONDA_ENV_PATH/etc/conda/activate.d/"
-cp scripts/clear_variables.sh "$CONDA_ENV_PATH/etc/conda/deactivate.d/"
-
-# Copy custom scripts
-cp scripts/GenFlow.sh "$CONDA_ENV_PATH/scripts/"
-cp scripts/datasets "$CONDA_ENV_PATH/scripts/"
-cp -r scripts/edirect "$CONDA_ENV_PATH/scripts/"
-cp scripts/run_script.sh "$CONDA_ENV_PATH/scripts/"
-cp scripts/ANI.R "$CONDA_ENV_PATH/scripts/"
-
-# Make the scripts executable
-chmod +x "$CONDA_ENV_PATH/etc/conda/activate.d/variables.sh"
-chmod +x "$CONDA_ENV_PATH/etc/conda/deactivate.d/clear_variables.sh"
-chmod +x "$CONDA_ENV_PATH/scripts/GenFlow.sh"
-chmod +x "$CONDA_ENV_PATH/scripts/datasets"
-chmod -R +x "$CONDA_ENV_PATH/scripts/edirect"
-chmod +x "$CONDA_ENV_PATH/scripts/run_script.sh"  # Make run_script.sh executable
-chmod +x "$CONDA_ENV_PATH/scripts/ANI.R"          # Optional: Make ANI.R executable if needed
+#
+chmod +x GenFlow.py
+chmod +x scripts/datasets
+chmod -R +x scripts/edirect
+chmod +x scripts/ANI.R
 
 # Install R libraries (Add your required packages here)
 Rscript -e "if (!requireNamespace('pheatmap', quietly = TRUE)) install.packages('pheatmap', repos='http://cran.r-project.org')"
