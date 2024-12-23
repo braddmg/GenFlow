d7cd8fe4d11b61eece4aa39d5eb0639d64a5a547
Bradd mendoza <56454173+braddmg@users.noreply.github.com>
2024-12-23 14:32:36 -0600

    Delete scripts/variables.sh

--- scripts/variables.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-#GenoFlow script
-CONDA_ENV_PATH=$(conda info --base)/envs/$(basename "$CONDA_PREFIX")
-export script=$CONDA_ENV_PATH/scripts/GenFlow.py
-alias GenFlow="python $script"
